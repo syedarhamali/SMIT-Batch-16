5ff072a22eb86bc39b95cb396a9aa3487f90db3d
Arham <arhamali63@gmail.com>
2025-05-15 17:48:17 +0500

    add js class-2 code

---
+++ js-class-2/index.js
@@ -0,0 +1,33 @@
+// alert("Hello world");
+
+// to create variable you have to write var 
+var person
+var age
+
+// alert(person) // undefined
+// alert(age) // undefined
+
+person = "Arham"
+
+person = "Tanzeel"
+
+person = "Hamza"
+
+person = 100
+
+age = 24
+// alert(person) // Arham
+// alert(age) // 24
+
+
+var numberOne = 12
+var numberTwo = 100
+
+alert(numberOne)
+alert(numberTwo)
+
+var result = numberOne / numberTwo
+alert(result)
+
+
+// '' jb bh ismy kuch likha hoga to wo string yani text hoga 
