942c65da7051220e819a412970b77e379d7c14f9
Arham <arhamali63@gmail.com>
2025-05-20 16:19:07 +0500

    js class-3 code

---
+++ js-class-3/index.js
@@ -0,0 +1,31 @@
+// var firstName = "ARHAM"
+// var age = 22
+
+// var numberOne = 1
+// var numberTwo = numberOne++
+
+// var result = numberOne + numberTwo
+
+// alert(result)
+// console.log(result)
+// document.write(numberTwo) 
+
+
+
+// var output = (1 + 2) * (10 / 5)
+
+// BODMAS (brackets open Divide , Multiply , Addition , subtraction)
+
+// console.log(output)
+
+
+
+// var firstName = +prompt("Enter your First Name??" , "Arham")
+// var lastName = +prompt("Enter your Last Name??" , "Ali")
+
+// console.log(firstName + lastName)
+// document.write(firstName + " " + lastName)
+
+document.write("Your first Number is 12" + "<br />")
+document.write("Your second Number is 12" + "<br />")
+document.write("The sum of number one and number two is 24 ")
