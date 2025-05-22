959b568c313f4628a21b198301323faa09a601c9
Arham <arhamali63@gmail.com>
2025-05-22 17:49:41 +0500

    js class-5 code

---
+++ js-class-5/index.js
@@ -0,0 +1,57 @@
+// var score = +prompt("Enter your score");
+
+// if (score >= 80) {
+//     document.write("you've got A-one grade")
+// } else if (score >= 70) {
+//     document.write("you've got A grade")
+// } else{
+//     document.write("you've got B grade")
+
+// }
+
+
+// var isPakistani = prompt("Are you pakistani??");
+// var userAge = +prompt("What's your age??");
+// var gender = prompt("what's your gender");
+
+// if (isPakistani === "yes" & gender === "male") {
+//     document.write("Congrats! you are eligible")
+// } else {
+//     document.write("sorry you are not eligible")
+// }
+
+
+// && if 999 condition is true and one condition is false then answer will be false
+// || if one condition is true and 999 condition is false then answer will be true
+
+// var student1 = "Ayaan"
+// var student2 = "Hamza"
+// var student3 = "Abdullah"
+// var students = [ "Ayaan" , "Hamza" , "Abdullah"]
+
+// // index value will always be start with 0 
+// // var students 
+
+// console.log(student1 )
+// console.log(student2 )
+// console.log(student3 )
+
+// console.log(students[0])
+// console.log(students[1]) // Hamza
+// console.log(students[2]) // Hamza
+
+
+var students = []
+
+console.log(students)
+
+students.push("Moiz" , "Tayyab" , 3 ,4 ,5)
+
+console.log(students)
+
+
+students[0] = "Moiz Ahmed"
+
+students[4] = "Tanzeel"
+
+console.log(students)
