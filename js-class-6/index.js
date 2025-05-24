f71ed1d5aa8e622f47e003aa0f2901215ecdac3a
Arham <arhamali63@gmail.com>
2025-05-24 18:00:25 +0500

    js-class-6 code

---
+++ js-class-6/index.js
@@ -0,0 +1,36 @@
+var cities = ["Karachi", "Peshawar", "Islamabad", "Rawalpindi", "Skardu", "Lahore"]
+
+// cities.push("Skardu") // added from the end
+
+// console.log(cities)
+
+// cities.pop() // remove from the end
+// cities.pop()
+
+
+// cities.unshift("Lahore")
+// cities.shift()
+
+// cities[2] = "Lahore"
+
+// cities.splice(6, 0 , "Multan")
+
+// var specifiCities = cities.slice(1, 3)
+
+// console.log(specifiCities) //it will copy from index 0 and before the 2nd index
+
+// console.log(cities)
+
+// console.log(cities[0])
+
+for (var i = 0; i <= 6; i++) {
+    if (cities[i] === "Islamabad") {
+        console.log(cities[i])
+        alert("yehi he saaf suthra sheher")
+    }
+}
+
+// console.log(cities[0])
+// console.log(cities[1])
+// console.log(cities[2])
+// console.log(cities[3])
