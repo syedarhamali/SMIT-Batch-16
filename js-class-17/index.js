f4a82f7fa48d2ca8dc5382161253710384288dc5
Arham <arhamali63@gmail.com>
2025-07-01 18:15:01 +0500

    class 18 js code

--- js-class-17/index.js
+++ js-class-17/index.js
@@ -9,30 +9,39 @@
 
 // console.log(userName)
 
+
+
+var person = {
+    name: "Arham",
+    age: 24,
+    nationality: "Pakistan"
+}
+
+
 const questions = [
     {
         id: 1,
         question: "What is your name",
-        options: ["Arham", "Taha"],
+        options: ["Arham", "Taha", "Hamza", "Affan"],
         correctAnswer: "Arham"
     },
     {
         id: 2,
         question: "What is your age",
-        options: [21, 22],
+        options: [21, 22, 23, 24],
         correctAnswer: 21
     },
     {
         id: 3,
         question: "What is your nationality",
-        options: [21, 22],
-        correctAnswer: 21
+        options: ["Afghani", "Pakistani", "Bangali", "British"],
+        correctAnswer: "Pakistani"
     },
     {
         id: 4,
         question: "What is your qualification",
-        options: [21, 22],
-        correctAnswer: 21
+        options: ["Jahil", "Matric", "Inter", "Graduate"],
+        correctAnswer: "Graduate"
     },
 ]
 let currentQuestion = 0
@@ -41,21 +50,34 @@ let currentQuestion = 0
 function showQuestions() {
     let questionElement = document.getElementById("question");
     questionElement.innerHTML = questions[currentQuestion].question
+
+    let optionsElement = document.getElementById("options")
+
+
+    optionsElement.innerHTML = ''
+
+    questions[currentQuestion].options
+
+    for (var i = 0; i < questions[currentQuestion].options.length; i++) {
+        optionsElement.innerHTML += `<div id="option" class="option">${questions[currentQuestion].options[i]}</div>`
+    }
 }
 
 showQuestions()
 
 
 function incrementQuestion() {
+    let nextQuestionButton = document.getElementById("nextBtn")
+    if (currentQuestion === questions.length - 2) {
+        nextQuestionButton.disabled = true
+    }
     currentQuestion++
     showQuestions()
 }
 
-// const myInfo = {
-//     myName : "Arham Ali",
-//     age: 23,
-//     nationality: "Pakistan"
-// }
+var testing = ["Arham", "Taha", "Hamza", "Affan"]
 
-// console.log(myInfo.myName)
-// console.log(myInfo["nationality"])
+
+testing.map((arham) => {
+    console.log(arham)
+})
