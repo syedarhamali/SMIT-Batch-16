a48c66a03f5b397c2e080384feb2ed650eddf533
Arham <arhamali63@gmail.com>
2025-07-08 17:56:55 +0500

    add js class 19 code

--- js-class-17/index.js
+++ js-class-17/index.js
@@ -60,25 +60,46 @@ function showQuestions() {
     questions[currentQuestion].options
 
     for (var i = 0; i < questions[currentQuestion].options.length; i++) {
-        optionsElement.innerHTML += `<div id="option" onClick="saveAnswer(event)" class="option">${questions[currentQuestion].options[i]}</div>`
+        optionsElement.innerHTML += `<div onClick="saveAnswer(event)" class="option">${questions[currentQuestion].options[i]}</div>`
     }
 }
 
 showQuestions()
 let scoreElement = document.getElementById("score")
 let userAnswer = null
+let nextQuestionButton = document.getElementById("nextBtn")
+let finishQuizButton = document.getElementById("finishQuiz")
+let quizContainer = document.getElementById("quiz-container")
+let restartQuizButton = document.getElementById("restartQuiz")
+let resultContainer = document.getElementById("result-container")
 
 function saveAnswer(event) {
-    console.log(event.target)
+
+    nextQuestionButton.disabled = false
     event.target.classList.add("active")
-    for
-    event.target.classList.remove("option")
+
+    let options = document.querySelectorAll(".option");
+    options.forEach((element) => {
+        if (event.target !== element) {
+            element.classList.remove("active")
+        }
+    })
+
+    // for(var i = 0 ; i < options.length ; i++){
+    //     if(event.target !== options[i]){
+    //        options[i].classList.remove("active")
+    //     }
+    // }
+
+    console.log(options)
+
+    // console.log(options)
     // console.log(event.target.classList)
     userAnswer = event.target.innerHTML
 }
 
 function incrementQuestion() {
-    let nextQuestionButton = document.getElementById("nextBtn")
+
     console.log(userAnswer)
     if (userAnswer == questions[currentQuestion].correctAnswer) {
         score = score + 10
@@ -86,9 +107,36 @@ function incrementQuestion() {
     currentQuestion++
     if (currentQuestion === questions.length - 1) {
         nextQuestionButton.disabled = true
+        nextQuestionButton.style.display = "none"
+        finishQuizButton.style.display = "block"
     }
     scoreElement.textContent = score
     showQuestions()
+    nextQuestionButton.disabled = true
+}
+
+
+function finishQuiz() {
+    if (userAnswer == questions[currentQuestion].correctAnswer) {
+        score = score + 10
+    }
+    scoreElement.textContent = score
+    quizContainer.style.display = 'none'
+    restartQuizButton.style.display = "block"
+    resultContainer.style.display = "block"
+}
+
+function restartQuiz() {
+    quizContainer.style.display = 'block'
+    currentQuestion = 0
+    showQuestions()
+    score = 0
+    scoreElement.textContent = score
+    nextQuestionButton.style.display = "block"
+    nextQuestionButton.disabled = true
+    restartQuizButton.style.display = "none"
+    finishQuizButton.style.display = "none"
+
 }
 
 // var testing = ["Arham", "Taha", "Hamza", "Affan"]
