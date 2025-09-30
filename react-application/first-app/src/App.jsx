555e93c6302e484e64236a6493bc29aff4997ab3
Arham <arhamali63@gmail.com>
2025-09-30 17:51:02 +0500

    use state work

--- react-application/first-app/src/App.jsx
+++ react-application/first-app/src/App.jsx
@@ -1,19 +1,12 @@
 //  component 
 
 import Signin from "./components/views/signin"
+import TodoList from "./components/views/todo-list"
 
 function App() { 
   return (
     // JavaScript XML 
-    <div>
-        My name is React 123
-        <Signin text="Hello React" />
-        {/* signin("text") */}
-        {/* properties (passing data to component) (props) (read-only value) */}
-        <Signin text="My new react value" number="0" />
-        <Signin text="Arham"/>
-        <Signin />
-    </div>
+    <TodoList />
     
   )
 }
---
+++ react-application/first-app/src/components/views/todo-list/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+
+function TodoList() {
+    // const [counter , setCounter] = useState({firstName: 'Arham' , lastName: 'Ali'})
+    // const lastName = "Ali"
+    // var counter = 10
+    // function increaseCounter(){
+    //     setCounter(counter + lastName)
+    // }
+    const [data, setData] = useState([{
+        "id": 1,
+        "title": "Essence Mascara Lash Princess",
+        "description": "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
+        "category": "beauty",
+        "price": 9.99
+    }])
+
+
+    const getProducts = async() =>{
+        const data = await fetch("https://dummyjson.com/products")
+        const response = await data.json()
+        setData(response.products)
+    }
+
+    getProducts()
+
+    return (
+        <div>
+            {data.map((element) => {
+                const {id , title , description , price} = element
+                return (
+                    <div id={id}>
+                        <h1>{title}</h1>
+                        <p>{description}</p>
+                        <p>{price}</p>
+                    </div>
+                )
+            })}
+
+            {/* <h1>Todo List</h1>
+
+            <p>Counter is <span>{counter}</span> </p>
+            <button onClick={increaseCounter}>Increase Counter</button> */}
+        </div>
+    )
+}
+
+export default TodoList
